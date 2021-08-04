dfac8ecfe5e190f21b94b556ca2a5b1c82b4dc83
Alex Manelis <amanelis@gmail.com>
2021-08-04 14:21:07 -0700

    Trying a secondary readDevice method

--- crypto/rand.go
+++ crypto/rand.go
@@ -14,47 +14,41 @@ var (
 	// load based on OS.
 	//
 	Devices = map[string]string{
-		"test":   "/dev/trandom",
-		"darwin": "/dev/urandom",
-		"linux":  "/dev/TrueRNG",
+		"r": "/dev/random",
+		"u": "/dev/urandom",
+		"t": "/dev/TrueRNG",
 	}
 )
 
 // Reader - Main interface that can be passed to almost all Golang crypto
 // implementations.
-//
 var Reader io.Reader
 
 // init - initialize the Reader with the most effective system based
 // rand generator.
-//
 func init() {
-	Reader = &devReader{name: Devices["darwin"]}
+	Reader = &devReader{name: Devices["u"]}
 }
 
 // devReader - satisfies reads by reading the file named name.
-//
 type devReader struct {
 	name string
 	f    io.Reader
 	mu   sync.Mutex
 }
 
-// Read - base read implementation for the reader. We here set our own rand
-// device based on the init/os above.
-//
-func (r *devReader) Read(b []byte) (n int, err error) {
+type hideAgainReader struct {
+	r io.Reader
+}
+
+var isEAGAIN func(error) bool
+
+func (r *devReader) ReadHardware(b []byte, d string) (n int, err error) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
 	if r.f == nil {
-		d := os.Getenv("RNG_DEVICE_PATH")
-
-		if d == "" {
-			d = "/dev/urandom"
-		}
-
-		fmt.Printf("block27/core/crypto/reader using [%s]\n", d)
+		fmt.Println("#block27/core/crypto : ReadHardware() using device: ", d)
 
 		f, err := os.Open(d)
 		if f == nil {
@@ -71,14 +65,33 @@ func (r *devReader) Read(b []byte) (n int, err error) {
 	return r.f.Read(b)
 }
 
-var isEAGAIN func(error) bool
+// Read - base read implementation for the reader. We here set our own rand
+// device based on the init/os above.
+func (r *devReader) Read(b []byte) (n int, err error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
 
-type hideAgainReader struct {
-	r io.Reader
+	if r.f == nil {
+		fmt.Println("#block27/core/crypto : Read() using device: ", r.name)
+		
+		f, err := os.Open(r.name)
+		if f == nil {
+			return 0, err
+		}
+
+		if runtime.GOOS == "plan9" {
+			r.f = f
+		} else {
+			r.f = bufio.NewReader(hideAgainReader{f})
+		}
+	}
+
+	return r.f.Read(b)
 }
 
 func (hr hideAgainReader) Read(p []byte) (n int, err error) {
 	n, err = hr.r.Read(p)
+
 	if err != nil && isEAGAIN != nil && isEAGAIN(err) {
 		err = nil
 	}
